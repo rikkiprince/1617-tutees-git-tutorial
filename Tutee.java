0f6b11afbccf9f38deb7f3cea7dd02a327f73a10
Rikki Prince <rfp@ecs.soton.ac.uk>
2017-03-10 14:32:05 +0000

    Added getters.

--- Tutee.java
+++ Tutee.java
@@ -1,7 +1,19 @@
 
 
 public class Tutee {
-	String name;
+	String currentName;
 	Float sleepiness; // 0-1
 	Boolean present;
+
+	public Tutee(String name) {
+		this.currentName = name;
+	}
+
+	public void setSleepiness(Float currentSleepiness) {
+		this.sleepiness = currentSleepiness;
+	}
+
+	public void changeName(String newName) {
+		this.currentName = newName;
+	}
 }
